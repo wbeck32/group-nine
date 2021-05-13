7f6026474f548552f3383df5c7cc685c38323d98
Wendy Beck <webeck@gmail.com>
2021-05-13 15:42:32 -0700

    got autocomplete to work correctly!

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,27 +3,22 @@ import React, {useState, useEffect} from 'react'
 import Search from './Search'
 import MovieList from './MovieList'
 
-const Main = props => {
-	console.log('props in main:', props);
-	// const {movieList} = props
+const Main = () => {
 	const [
-		movies,
-		setMovies
+		movieList,
+		setMovieList
 	] = useState([])
 	
-	const [
-		query,
-		setQuery
-	] = useState("")
 
-	
+
 	const handleChange = e => {
+		console.log('e:', e);
 		console.log('e.target.value: in Main', e.target.value);
-		setQuery(encodeURI(e.target.value))
+		let query = e.target.value
+		encodeURI(query)
 		// console.log('queryString:', query);
-		return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`)
+		return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query="${query}"`)
 			.then(response => {
-				console.log('response:', response);
 				if (response.status === 200) {
 					return response.json();
 				} else {
@@ -31,7 +26,7 @@ const Main = props => {
 				}
 			})
 			.then(response => {
-				setMovies(response.results)
+				setMovieList(response.results)
 			})
 			.catch(error => {
 				console.error(error);
@@ -47,8 +42,8 @@ const Main = props => {
 
 	return (
 		<>
-			<Search movieList={movies} handleChange={e=>handleChange(e)} handleOptionLabel={e=>handleOptionLabel(e)} query={query}/>
-			<MovieList movieList={movies} />
+			<Search movieList={movieList} handleOptionLabel={e=>handleOptionLabel(e)} handleChange={e=>handleChange(e)}/>
+			<MovieList movieList={movieList} />
 		</>
 	)
 }
