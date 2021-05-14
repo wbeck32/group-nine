e0029a767cce012c8b55c518b6b708dbb246a262
Wendy Beck <webeck@gmail.com>
2021-05-13 17:15:52 -0700

    adding validation for query length

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,29 +18,29 @@ const Main = () => {
 	const [
 		currentPage,
 		setCurrentPage
-	] = useState(0)
+	] = useState(1)
 	
 	const handleChange = e => {
-		console.log('e.target.value: in Main', e.target.value);
-		let query = e.target.value
-		encodeURI(query)
-		// console.log('queryString:', query);
-		return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${currentPage}&include_adult=false&query=${query}`)
-			.then(response => {
-				if (response.status === 200) {
-					return response.json();
-				} else {
-					throw new Error('Something went wrong on api server!');
-				}
-			})
-			.then(response => {
-				setCurrentPage(currentPage + 1)
-				setNumPages(response.total_pages)
-				setMovieList(response.results)
-			})
-			.catch(error => {
-				console.error(error);
-			})
+		if(e.target.value.length > 0){
+			let query = e.target.value
+			encodeURI(query)
+			return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${currentPage}&include_adult=false&query=${query}`)
+				.then(response => {
+					if (response.status === 200) {
+						return response.json();
+					} else {
+						throw new Error('Something went wrong on api server!');
+					}
+				})
+				.then(response => {
+					setCurrentPage(currentPage + 1)
+					setNumPages(response.total_pages)
+					setMovieList(response.results)
+				})
+				.catch(error => {
+					console.error(error);
+				})
+		}
 	}
 
 	const handleOptionLabel = option => {
@@ -53,7 +53,7 @@ const Main = () => {
 	return (
 		<>
 			<Search movieList={movieList} handleOptionLabel={e=>handleOptionLabel(e)} handleChange={e=>handleChange(e)}/>
-			<MovieList movieList={movieList} numPages={numPages}/>
+			<MovieList movieList={movieList} handleChange={e=>handleChange(e)} currentPage={currentPage} numPages={numPages}/>
 		</>
 	)
 }
